d67969d12fb5c325f4e78521ed4f2124ac3f4b3f
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-09-27 01:40:28 +0300

    Initial commit - final fix

---
+++ server/testing/conftest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pytest
+from app import app, db
+
+def pytest_itemcollected(item):
+    par = item.parent.obj
+    node = item.obj
+    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
+    suf = node.__doc__.strip() if node.__doc__ else node.__name__
+    if pref or suf:
+        item._nodeid = ' '.join((pref, suf))
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_database():
+    with app.app_context():
+        db.create_all()
+    yield
+    with app.app_context():
+        db.drop_all()
