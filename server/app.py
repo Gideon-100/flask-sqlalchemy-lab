4fbe219c0187baa6ca2633b7d74d7adf0ab961f2
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-09-27 02:06:19 +0300

    chnages

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate
 from models import db
 
 app = Flask(__name__)
+
+#configure the database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
